010bdacce91117d0e515391f2c9161c54496ebe8
luiz dos reis <luizdosreis@gmail.com>
2017-04-18 17:26:11 -0300

    criando o contador de palavras e caracteres

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,5 +1,12 @@
 qtPalavras = $(".frase").text().split(" ").length;
 $('#qt_palavras').text(qtPalavras);
 
+var campo = $('.campo-digitacao');
+
+campo.on('input', function() {
+    $('#contador-palavras').text(campo.val().split(/\S+/).length -1);
+    $('#contador-caracter').text(campo.val().length);
+});
+
 
 
