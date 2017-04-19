57ca03ac3711438db221226f092dfaa2b5c8102e
luiz dos reis <luizdosreis@gmail.com>
2017-04-19 17:58:59 -0300

    criando o botao de reiniciar e estruturando o main.js para usar funcoes

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,27 +1,59 @@
-qtPalavras = $(".frase").text().split(" ").length;
-$('#qt_palavras').text(qtPalavras);
-
+var tempoInicial = $('#tempo').text();
 var campo = $('.campo-digitacao');
 
-campo.on('input', function() {
-    $('#contador-palavras').text(campo.val().split(/\S+/).length -1);
-    $('#contador-caracter').text(campo.val().length);
+
+$(function(){
+    atualizaTamanhoFrase();
+    atualizaContadores();
+    cronometro();
+    $('#botao-reiniciar').click(reiniciaJogo);
+
 });
 
+function atualizaTamanhoFrase(){
+    var qtPalavras = $(".frase").text().split(" ").length;
+    $('#qt_palavras').text(qtPalavras);
+}
 
-campo.one('focus',function(){
-     var tempo = $('#tempo').text();
-     var cronometroId  = setInterval(function(){
+function atualizaContadores(){
+    campo.on('input', function() {
+        $('#contador-palavras').text(campo.val().split(/\S+/).length -1);
+        $('#contador-caracter').text(campo.val().length);
+    });
+}
+
+function cronometro(){
+    campo.one('focus',function(){
+        var tempo = tempoInicial;
+        var cronometroId  = setInterval(function(){
             tempo--;
             $('#tempo').text(tempo);
             if(tempo < 1){
-                campo.attr('disabled', 'true');
+                campo.attr('disabled', true);
                 clearInterval(cronometroId);
             }
         },1000);
+    });
+};
+
+function reiniciaJogo(){
+        campo.attr('disabled', false);
+        campo.val('');
+        $('#contador-palavras').text('0');
+        $('#contador-caracter').text('0');
+        $('#tempo').text( tempoInicial);
+        cronometro();
+};
 
 
-});
+
+
+
+
+
+
+
+
 
 
 
