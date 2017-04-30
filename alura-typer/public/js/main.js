59183508643fc5981f42fb63f24f6dd215f016a3
luiz dos reis <luizdosreis@gmail.com>
2017-04-30 15:21:30 -0300

    criando o botao remover

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -47,13 +47,42 @@ function inserePlacar(){
             var palavras = $('#contador-palavras').text();
             var usuario = 'luiz henrique';
 
-            linha = '<tr>'+
-                            '<td class="center">'+usuario+'</td>'+
-                            '<td class="center">'+palavras+'</td>'+
-                        '</tr>';
+            linha = criarLinha(usuario,palavras);
+
+            linha.find('.remover').click(removerLinhas);
+
+
 
             corpoTabela.prepend(linha);
 }
+
+
+function criarLinha(usuario,palavras){
+    linha = $('<tr>');
+    colunaUsuario = $('<td>').text(usuario);
+    colunaPalavras = $('<td>').text(palavras);
+    colunaRemover = $('<td>');
+
+    link = $('<a>').addClass('remover','center');
+
+    icone = $('<i>').addClass('material-icons').text('delete');
+
+    link.append(icone);
+
+    colunaRemover.append(link);
+
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+
+
+function removerLinhas(event) {
+    event.preventDefault();
+    $(this).parents('tr').remove();
+};
 
 function comparaPalavras(frase,digitado){
     if(frase == digitado){
