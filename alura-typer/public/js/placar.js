f54d0c0890d5401bf6e3cb4ff78fe14706239194
luiz dos reis <luizdosreis@gmail.com>
2017-04-30 15:55:10 -0300

    separando o codigo referente ao placar

---
+++ alura-typer/public/js/placar.js
@@ -0,0 +1,39 @@
+function inserePlacar(){
+            var corpoTabela = $('.placar').find('tbody');
+            var palavras = $('#contador-palavras').text();
+            var usuario = 'luiz henrique';
+
+            linha = criarLinha(usuario,palavras);
+
+            linha.find('.remover').click(removerLinhas);
+
+            corpoTabela.prepend(linha);
+}
+
+
+function criarLinha(usuario,palavras){
+    linha = $('<tr>');
+    colunaUsuario = $('<td>').text(usuario);
+    colunaPalavras = $('<td>').text(palavras);
+    colunaRemover = $('<td>');
+
+    link = $('<a>').addClass('remover','center');
+
+    icone = $('<i>').addClass('material-icons').text('delete');
+
+    link.append(icone);
+
+    colunaRemover.append(link);
+
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+
+
+function removerLinhas(event) {
+    event.preventDefault();
+    $(this).parents('tr').remove();
+};
