ffacf1f71efe520ccd51817dd0fcaf4acfd4feff
luizdosreis <luizdosreis@gmail.com>
2017-06-24 15:50:49 -0300

    criando o busca frase por id

--- alura-typer/public/js/frase.js
+++ alura-typer/public/js/frase.js
@@ -1,4 +1,5 @@
 $('#botao-frase').click(fraseAleatoria);
+$('#botao-frase-id').click(buscaFrase);
 
 function fraseAleatoria(){
   $('#spinner').toggle();
@@ -19,4 +20,27 @@ function trocaFraseAleatoria(data){
   $('.frase').text(data[numeroAleatorio].texto);
   atualizaTamanhoFrase();
   atualizaTempoInicial(data[numeroAleatorio].tempo);
+}
+
+function buscaFrase(){
+  $('#spinner').toggle();
+  let fraseId = $('#frase-id').val();
+  let dados = {id:fraseId};
+
+  $.get("http://localhost:3000/frases",dados,trocaFrase)
+  .fail(function(){
+    $('#erro').toggle();
+    setTimeout(function(){
+        $('#erro').toggle();
+    },2000);
+  })
+  .always(function(){
+    $('#spinner').toggle();
+  });
+}
+
+function trocaFrase(data){
+  $('.frase').text(data.texto);
+  atualizaTamanhoFrase();
+  atualizaTempoInicial(data.tempo);
 }
